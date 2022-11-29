204cffeef9e69166569bb7aa5818f8583d5148e6
sedziah <sedziah@gmail.com>
2022-11-29 15:32:16 -0800

    task 6 updated

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(10):
         if i < j:
-            print("{:d}{:d}".format(i, j), end=" ")
+            print("{:d}{:d}".format(i, j), end="")
             if i < 8:
-                print(", ", end=" ")
+                print(", ", end="")

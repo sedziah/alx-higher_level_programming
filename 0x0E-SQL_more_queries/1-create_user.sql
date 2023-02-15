2cd10707b22094a2b219416923ac1d015ffaa264
Solomon Edziah <68519058+sedziah@users.noreply.github.com>
2023-02-15 22:19:42 +0000

    Task files aded

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,7 @@
--- A script that creates a user
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-
-/* Grant all privileges to user */
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+-- Creates the user user_0d_1 with all privileges.
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost';
--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
--- A script that creates a table
-CREATE TABLE IF NOT EXISTS force_name (
-	id INT,
-	name VARCHAR(256) NOT NULL
+-- Creates the table force_name.
+CREATE TABLE IF NOT EXISTS `force_name` (
+    `id`   INT,
+    `name` VARCHAR(256) NOT NULL
 );
